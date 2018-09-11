e5da0ac6f9db8375a3382e4aeed9f10f268ac456
Radhika Santapuri <radhika.santapuri@gmail.com>
2018-09-11 10:52:39 -0500

    Meme generator Lesson 1

---
+++ MemeMe/MemeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by radhika santapuri on 9/10/18.
+//  Copyright © 2018 radhika santapuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
